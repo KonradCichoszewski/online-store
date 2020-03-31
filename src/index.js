0be9d1e74404744f2df6067e8704e109eb10d5d5
cichoszewicz <cichoszewicz@gmail.com>
2020-03-31 23:37:47 +0200

    Improved navbar display

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom'
 
 import store from './redux/store.js'
 import Root from './Root.js'
+import './index.css'
 
 ReactDOM.render(<Root store={store}/>, document.getElementById('root'));
